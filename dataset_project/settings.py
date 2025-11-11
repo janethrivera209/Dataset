5f2a57f313df6c89caaf521d517deae74d7edb8e
janethrivera209 <riverarobledojaneth@gmail.com>
2025-11-10 22:09:04 -0600

    Descripción de los cambios que hiciste

--- dataset_project/settings.py
+++ dataset_project/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i25&jw545%xa0lt%!8kg94cb-jj%38d&5#$13(jqopuad+au92'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tu-app.onrender.com', 'localhost']
+
 
 
 # Application definition
